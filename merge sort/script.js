4f15a3dc60b4299b6b1e7f187d25a1826ca8c78a
Asryan Babken <asrayn.babken@yandex.ru>
2020-03-19 18:11:48 +0400

    Comments added

--- merge sort/script.js
+++ merge sort/script.js
@@ -1,33 +1,40 @@
+// Для начало нужно написать первую функцию, которая принимает на входе массив, проверяет и делит на две части, впоследствии вызывает следующую функцию, в виде рекурсии. В итоге создается стек вызовов, где мы имеем в итоге массивы по одному элементу. Дальше мы сравниваем два массива с одиночными элементами, вставляя наименьший в отсортированный массив, а остальную часть в последствии в этот же массив, чтобы обработать в следующем стеке.
+
 const mergeSort = arr => {
 
-    if (!arr || !arr.length) return null;                               // Checking the data
+    if (!arr || !arr.length) return null;                               // Checking the data  
+                                                                        // Проверяем данные
 
     if (arr.length <= 1) return arr;                                    // Exit from recursion if an array has one element
+                                                                        // Условие выхода из рекурсии
+    const middle = Math.floor(arr.length/2);                            // Getting the center of an array
+                                                                        // Центр массива
 
-    const middle = Math.floor(arr.length/2);                              // Getting the center of an array
-
-    const first = arr.slice(0, middle);                                   // The first part of an array
-
-    const second = arr.slice(middle);                                     // The second part of an array
+    const first = arr.slice(0, middle);                                 // The first part of an array
+                                                                        // Первый массив
+    const second = arr.slice(middle);                                   // The second part of an array
+                                                                        // Второй массив
 
     return merge(mergeSort(first), mergeSort(second));                  // The start of a recursion
+                                                                        // Вызов функции слияния методом рекурсии
 }
 const merge = (first, second) => {
 
     let arr = [];                                                       // Our sorted an array
-
+                                                                        // Массив для заполнения
     let i = 0,                                                          // Our indexes for cycle 
-        k = 0;
+        k = 0;                                                          // Наши индексы для цикла
 
-    while (i < first.length, k < second.length) {                       // The cycle where we exit if one of the array is empty when one of the indexes change
+    while (i < first.length && k < second.length) {                     // The cycle where we exit if one of the array is empty when one of the indexes change 
         arr.push((first[i] < second[k]) ? first[i++] : second[k++])     // Pushing in the array the element which is less and that element's index changes
-    }
-
+    }                                                                   // Здесь начинается цикл с условием если у одного из конечных массивов есть элемент, тогда внутри просходит заливка в массив
+                                                                        // Внутри цикла сравнивают два элемента двух массивов, добавляя в массив наименьший
     return [                                                            // Return an array of sorted elements with the rest of an first or second array
-        ...arr,
+        ...arr,                                                         // Возвращаю сортированный массив и остаток от массивов сравнения
         ...first.slice(i),
         ...second.slice(k)
     ]
 }
 
 mergeSort([8,5,6,7,3,2,1,4,9]);
+
