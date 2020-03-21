dd0b4b7a64f8e9df94ac690865bb94dfd250f5d7
Asryan Babken <asrayn.babken@yandex.ru>
2020-03-21 14:12:14 +0400

    Last errors

--- fibonacci/script.js
+++ fibonacci/script.js
@@ -1,26 +1,31 @@
 // Всем известный алгоритм чисел Фибоначчи
 const fibonacci = num => {
+    
     const fib = [1];                                // Начальный массив
     
     if (!num || num <= 0) return null;              // Случай с неправильными данными
     
     if (num === 1) return fib;                      // Случай с единицой
     
-    let iterationNumber = n - 1;                    // Количество итераций
+    let iterationNumber = num - 1;                  // Количество итераций
     
     let currentVal = 1;                             // Текущее число
+
     let previousVal = 0;                            // Предыдущее число
 
     while (iterationNumber) {                       // Цикл с условием положительности переменной
-        
+                
         currentVal += previousVal;                  // Увеличиваем текущее значение на предыдущее
 
         previousVal = currentVal - previousVal;     // Запоминаем предыдущее число как разницу текущего и предыдущего
 
+        fib.push(currentVal);                       // Заливаем в массив
+
         --iterationNumber;                          // Уменьшаем число итераций
+
     }                               
     
     return fib;                                     // Возвращаем результат
 }
 
-console.log(fibonacci(77))
+console.log(fibonacci(7))
